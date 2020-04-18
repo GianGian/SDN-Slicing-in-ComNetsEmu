ec00efae63224c531cd38eac97c22b18d6e5c165
GianGian <gianlorenzomoser@alice.it>
2020-04-18 18:24:09 +0200

    controller

--- app/SDN-Slicing/Slice_Topo.py
+++ app/SDN-Slicing/Slice_Topo.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-
-
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import OVSKernelSwitch, RemoteController
@@ -14,27 +12,28 @@ DELAY="10ms"
 
 class Topology(Topo):
     def __init__(self):
-        Topo.init(self)
+        Topo.__init__(self)
 
         #Create switches
-        info("*** Add Switches\n")
+        #info("*** Add Switches\n")
         switch1=self.addSwitch("s1")
         switch2=self.addSwitch("s2")
         switch3=self.addSwitch("s3")
         switch4=self.addSwitch("s4")
 
         #Create Hosts
-        info("*** Add Hosts\n")
-        host1=self.addDockerhost("h1", dimage="dev_test", ip="192.0.0.1", docker_args={"hostname"="h1"},)
-        host2=self.addDockerhost("h2", dimage="dev_test", ip="192.0.0.2", docker_args={"hostname"="h2"},)
-        host3=self.addDockerhost("h3", dimage="dev_test", ip="192.0.0.3", docker_args={"hostname"="h3"},)
-        host4=self.addDockerhost("h4", dimage="dev_test", ip="192.0.0.4", docker_args={"hostname"="h4"},)
-        host5=self.addDockerhost("h5", dimage="dev_test", ip="192.0.0.5", docker_args={"hostname"="h5"},)
-        host6=self.addDockerhost("h6", dimage="dev_test", ip="192.0.0.6", docker_args={"hostname"="h6"},)
-        host7=self.addDockerhost("h7", dimage="dev_test", ip="192.0.0.7", docker_args={"hostname"="h7"},)
+        #info("*** Add Hosts\n")
+	#da host a dockerhost
+        host1=self.addHost("h1", dimage="dev_test", ip="192.0.0.1", docker_args={"hostname":"h1"},)
+        host2=self.addHost("h2", dimage="dev_test", ip="192.0.0.2", docker_args={"hostname":"h2"},)
+        host3=self.addHost("h3", dimage="dev_test", ip="192.0.0.3", docker_args={"hostname":"h3"},)
+        host4=self.addHost("h4", dimage="dev_test", ip="192.0.0.4", docker_args={"hostname":"h4"},)
+        host5=self.addHost("h5", dimage="dev_test", ip="192.0.0.5", docker_args={"hostname":"h5"},)
+        host6=self.addHost("h6", dimage="dev_test", ip="192.0.0.6", docker_args={"hostname":"h6"},)
+        host7=self.addHost("h7", dimage="dev_test", ip="192.0.0.7", docker_args={"hostname":"h7"},)
 
         #Creating Links
-        info("*** Add Links\n")
+        #info("*** Add Links\n")
         self.addLink("h1","s1", bw=B1)
         self.addLink("h2","s1", bw=B1)
         self.addLink("h3","s1", bw=B1)
@@ -49,7 +48,27 @@ class Topology(Topo):
         self.addLink("s4","h7", bw=B1)
 
         #Create Controller
-        info("*** Add controller\n")
-        net.addController("c0")
+        #info("*** Add controller\n")
+       # net.addController("c0")
+
+topos = {"networkslicingtopo": (lambda: Topology())}
+
+if __name__ == "__main__":
+    topo = Topology()
+    net = Mininet(
+        topo=topo,
+        switch=OVSKernelSwitch,
+        build=False,
+        autoSetMacs=True,
+        autoStaticArp=True,
+        link=TCLink,
+    )
+    controller = RemoteController("c1", ip="127.0.0.1", port=6633)
+    net.addController(controller)
+    net.build()
+    net.start()
+    CLI(net)
+    net.stop()
+
 
         
---
+++ app/SDN-Slicing/controller.py
@@ -0,0 +1,19 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
+from ryu.lib.packet import udp
+from ryu.lib.packet import tcp
+from ryu.lib.packet import icmp
+
+
+#####settare porte con link
+
+#nuovo flusso da mandare a controller
+
+#settare risposta a packet-in
+#definire flussi in base alla porta
